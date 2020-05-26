4a1723cbb587e10ba7d78a90f5911d141038adf4
LiliaRibac <ribac.lilia@gmai.com>
2020-05-25 17:12:26 -0700

    able to add a quote

---
+++ quotes-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <new-quote @quoteAdded="newQuote"></new-quote>
+    <quote-grid :quotes="quotes" ></quote-grid>
+  </div>
+</template>
+
+<script>
+import NewQuote from './components/NewQuote.vue'
+import QuoteGrid from './components/QuoteGrid.vue'
+
+export default {
+  components:{
+    QuoteGrid,
+    NewQuote
+  },
+  data() {
+    return {
+      quotes: ['Here I am '],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quote){
+      this.quotes.push(quote)
+    }
+  },
+};
+</script>
+
+<style></style>
